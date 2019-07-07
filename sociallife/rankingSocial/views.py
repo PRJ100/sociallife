64d3c734eb822a0e23de75b8ebed2e0afa514e5f
Romario <donprj100@gmail.com>
2019-07-07 00:41:10 -0300

    criaçao de rankingSocial e home

--- sociallife/rankingSocial/views.py
+++ sociallife/rankingSocial/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'rankingSocial/lista.html'
+    context_object_name = 'contexto'
+    def get_queryset(self):
+        pass
